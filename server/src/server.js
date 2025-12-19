a361a98bae840a1a1375451a2dd7268c558d9992
Tanish Chordia <tanishchordia5@gmail.com>
2025-12-19 18:31:31 +0530

    Setup MongoDB Atlas connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,10 @@
 const app = require("./app");
 require("dotenv").config();
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
