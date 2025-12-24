2d546e7f05d3a43b75c474344e9efb661c2df4b5
Tanish Chordia <tanishchordia5@gmail.com>
2025-12-24 16:58:55 +0530

    dataType change in chat modal

--- server/src/modules/chat/chat.model.js
+++ server/src/modules/chat/chat.model.js
@@ -8,7 +8,7 @@ const messageSchema = new mongoose.Schema(
             required : true
         },
         text : {
-            type : string,
+            type : String,
             required : true
         }
     },
